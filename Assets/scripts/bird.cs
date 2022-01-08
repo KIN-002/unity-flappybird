70a9415180cc0c727ef8d4551ad93d6c85af0e4c
KIN_002 <67863453+LiKING002@users.noreply.github.com>
2022-01-09 06:33:12 +0800

    Add files via upload

---
+++ Assets/scripts/bird.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour {
+	public float upForce = 200f;
+	private bool isDead = false;
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();		
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isDead == false)
+		{
+			if (Input.GetMouseButtonDown (0))
+			{
+				rb2d.velocity = Vector2.zero; //設定速度為0
+				rb2d.AddForce(new Vector2(0,upForce));  //加向上進力
+				anim.SetTrigger("Flap");
+			}
+		}
+	}
+	void OnCollisionEnter2D()
+	{
+		rb2d.velocity = Vector2.zero;
+		isDead = true;
+		anim.SetTrigger ("Die");
+		gameControl.instance.BirdDied ();
+	}
+}
---
+++ Assets/scripts/gameControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameControl : MonoBehaviour {
+
+	public static gameControl instance;
+	public GameObject gameOverText;
+	public bool gameOver = false;
+	public float scrollSpeed = -1.5f;
+
+
+	void Awake()   //Awake是在start之前執行, 
+	{
+		if (instance == null) //如果沒有物件執行
+		{
+			instance = this;  //執行這個
+		}
+		else if (instance != this)  //如果己有一個物件執行
+		{
+			Destroy (gameObject);  //就毁滅這個物件
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameOver == true && Input.GetMouseButtonDown (0))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+		
+	}
+
+	public void BirdDied()
+	{
+		gameOverText.SetActive (true);  
+		gameOver = true;
+	}
+
+	public void BirdScored()
+	{
+		if (gameOver) 
+		{
+			return;
+		}
+
+
+	}
+}
